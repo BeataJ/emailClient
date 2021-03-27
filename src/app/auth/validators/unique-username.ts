6aea01f7d2f8f77dc90b02407b5201098e20009f
beataj <beata@emc2wave.com>
2021-03-26 18:25:49 -0700

    add AsyncValidator

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AsyncValidator } from '@angular/forms';
 
 @Injectable({providedIn: 'root'})
-export class UniqueUsername {
+export class UniqueUsername implements AsyncValidator{
     constructor(private http: HttpClient) {}
 }
