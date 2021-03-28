67bff2acf3a9e0b342169a365eb65de6351e768a
beataj <beata@emc2wave.com>
2021-03-27 22:30:47 -0700

    import map operator

--- src/app/auth/validators/unique-username.ts
+++ src/app/auth/validators/unique-username.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AsyncValidator, FormControl } from '@angular/forms';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class UniqueUsername implements AsyncValidator{
