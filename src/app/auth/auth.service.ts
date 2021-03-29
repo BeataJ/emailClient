345fc6cfa1b233fec67e48d220adbbe65e999bb5
beataj <beata@emc2wave.com>
2021-03-29 15:14:33 -0700

    add http to service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  usernameAvailable(username: string) {
+    return this.http.post<any>('https://api.angular-email.com/auth/username', {
+      username
+    })
+  }
 }
