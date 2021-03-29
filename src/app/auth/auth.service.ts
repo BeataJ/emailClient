cb8259b89b4dafbc9598e1391e4dc6ff8dc786e7
beataj <beata@emc2wave.com>
2021-03-29 15:27:03 -0700

    create interface UsernameAvailableResponse

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface UsernameAvailableResponse {
+  available: boolean
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,7 +13,7 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   usernameAvailable(username: string) {
-    return this.http.post<{ available:boolean }>('https://api.angular-email.com/auth/username', {
+    return this.http.post<UsernameAvailableResponse>('https://api.angular-email.com/auth/username', {
       username
     })
   }
