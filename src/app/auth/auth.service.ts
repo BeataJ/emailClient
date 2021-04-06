9599face2381115aec8a6029396e7b81d669d8ad
beataj <beata@emc2wave.com>
2021-04-06 14:08:06 -0700

    delete withCredentials: true from auth.service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -33,9 +33,7 @@ export class AuthService {
   }
 
   signup = (credentials: SignupCredentials) => {
-    return this.http.post<SignupResponse>(`${this.rootUrl}/auth/signup`, credentials, {
-      withCredentials: true
-    })
+    return this.http.post<SignupResponse>(`${this.rootUrl}/auth/signup`, credentials)
      .pipe(
        tap(() => {
          this.signedin$.next(true)
@@ -44,9 +42,7 @@ export class AuthService {
   }
 
   checkAuth = () => {
-    return this.http.get(`${this.rootUrl}/auth/signedin`, {
-      withCredentials: true
-    })
+    return this.http.get(`${this.rootUrl}/auth/signedin`)
      .pipe(
        tap(response => {
          console.log(response)
