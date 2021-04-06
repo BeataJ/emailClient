ed394f29beac02839771b3f3585fbd8e16560488
beataj <beata@emc2wave.com>
2021-04-06 14:54:49 -0700

    add filter operators

--- src/app/auth/auth-http-interceptor.ts
+++ src/app/auth/auth-http-interceptor.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpEventType } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, filter } from 'rxjs/operators';
 
 @Injectable()
 export class AuthHttpInterceptor implements HttpInterceptor{
@@ -12,14 +12,9 @@ export class AuthHttpInterceptor implements HttpInterceptor{
         
         return next.handle(modifiedReq)
             .pipe(
+                filter(val => val.type === HttpEventType.Sent),
                 tap(val => {
-                    if( val.type === HttpEventType.Sent ) {
-                        console.log('Request was sent to server')
-                    }
-
-                    if( val.type === HttpEventType.Response) {
-                        console.log('Got a response from the API', val)
-                    }
+                    console.log('sent the request')
                 })
             )
     }
