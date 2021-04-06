006d9607d722f08da52fbe8c86efa9dea4c350eb
beataj <beata@emc2wave.com>
2021-04-06 14:47:28 -0700

    add HttpEventType

--- src/app/auth/auth-http-interceptor.ts
+++ src/app/auth/auth-http-interceptor.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpEventType } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 
@@ -13,7 +13,13 @@ export class AuthHttpInterceptor implements HttpInterceptor{
         return next.handle(modifiedReq)
             .pipe(
                 tap(val => {
-                    console.log(val)
+                    if( val.type === HttpEventType.Sent ) {
+                        console.log('Request was sent to server')
+                    }
+
+                    if( val.type === HttpEventType.Response) {
+                        console.log('Got a response from the API', val)
+                    }
                 })
             )
     }
