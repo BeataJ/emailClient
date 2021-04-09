56f8749fe8899938f1f099db956719ac6ce3dc74
beataj <beata@emc2wave.com>
2021-04-08 22:58:20 -0700

    add Authservice take skipWhile

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import { CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import { take, skipWhile } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
+  constructor(private authService: AuthService) {}
+
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return new Observable(subscriber => {
-      subscriber.next(true);
+     return this.authService.signedin$.pipe(
+      skipWhile(value => value === null),
+      take(1)
+     );
      
-    });
-  }
+    };
+  
 }
