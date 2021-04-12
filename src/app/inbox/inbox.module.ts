e956a0a67a013dd97c47b98706bfe224fd22b449
beataj <beata@emc2wave.com>
2021-04-11 22:04:38 -0700

    create Email-index comp

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -5,10 +5,11 @@ import { InboxRoutingModule } from './inbox-routing.module';
 import { HomeComponent } from './home/home.component';
 import { EmailCreateComponent } from './email-create/email-create.component';
 import { EmailReplayComponent } from './email-replay/email-replay.component';
+import { EmailIndexComponent } from './email-index/email-index.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, EmailCreateComponent, EmailReplayComponent],
+  declarations: [HomeComponent, EmailCreateComponent, EmailReplayComponent, EmailIndexComponent],
   imports: [
     CommonModule,
     InboxRoutingModule
