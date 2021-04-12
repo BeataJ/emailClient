e34ea830b22508abd9bf6790d0f411833602fb2e
beataj <beata@emc2wave.com>
2021-04-11 22:05:26 -0700

    create email-show comp

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -6,10 +6,11 @@ import { HomeComponent } from './home/home.component';
 import { EmailCreateComponent } from './email-create/email-create.component';
 import { EmailReplayComponent } from './email-replay/email-replay.component';
 import { EmailIndexComponent } from './email-index/email-index.component';
+import { EmailShowComponent } from './email-show/email-show.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, EmailCreateComponent, EmailReplayComponent, EmailIndexComponent],
+  declarations: [HomeComponent, EmailCreateComponent, EmailReplayComponent, EmailIndexComponent, EmailShowComponent],
   imports: [
     CommonModule,
     InboxRoutingModule
