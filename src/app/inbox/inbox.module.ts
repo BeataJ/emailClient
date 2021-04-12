eaafeb62be8cd5759a687536a98237bf0918cdba
beataj <beata@emc2wave.com>
2021-04-11 22:04:09 -0700

    create Email-relpy conmp

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { InboxRoutingModule } from './inbox-routing.module';
 import { HomeComponent } from './home/home.component';
 import { EmailCreateComponent } from './email-create/email-create.component';
+import { EmailReplayComponent } from './email-replay/email-replay.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, EmailCreateComponent],
+  declarations: [HomeComponent, EmailCreateComponent, EmailReplayComponent],
   imports: [
     CommonModule,
     InboxRoutingModule
