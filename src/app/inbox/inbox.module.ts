1d028af5e1fccf780b358c6b69004c4c2eaa6aaf
beataj <beata@emc2wave.com>
2021-04-11 22:09:30 -0700

    delete email-replay from module

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -4,13 +4,13 @@ import { CommonModule } from '@angular/common';
 import { InboxRoutingModule } from './inbox-routing.module';
 import { HomeComponent } from './home/home.component';
 import { EmailCreateComponent } from './email-create/email-create.component';
-import { EmailReplayComponent } from './email-replay/email-replay.component';
+
 import { EmailIndexComponent } from './email-index/email-index.component';
 import { EmailShowComponent } from './email-show/email-show.component';
 
 
 @NgModule({
-  declarations: [HomeComponent, EmailCreateComponent, EmailReplayComponent, EmailIndexComponent, EmailShowComponent],
+  declarations: [HomeComponent, EmailCreateComponent, EmailIndexComponent, EmailShowComponent],
   imports: [
     CommonModule,
     InboxRoutingModule
