38941d387063bc5806f44d9f1413caaf2e15994f
beataj <beata@emc2wave.com>
2021-04-12 17:02:14 -0700

    create interface EmailSumary

--- src/app/inbox/email.service.ts
+++ src/app/inbox/email.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface EmailSumary {
+  id: string;
+  subject: string;
+  from: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,4 +14,8 @@ export class EmailService {
   rootUrl = 'https://api.angular-email.com'
 
   constructor(private http: HttpClient) { }
+
+  getEmails = () => {
+    return this.http.get(`${this.rootUrl}/emails`)
+  }
 }
