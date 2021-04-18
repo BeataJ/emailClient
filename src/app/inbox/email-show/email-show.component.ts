d77a53c59d6d3f28600cd0dda946348a7b88a6d2
beataj <beata@emc2wave.com>
2021-04-17 18:07:08 -0700

    import switchMap

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 import { EmailService } from '../email.service';
 
 @Component({
