a58ffd23f09b346373b073c2985bfd66fe4db26f
beataj <beata@emc2wave.com>
2021-04-19 21:32:45 -0700

    add EMPTY

--- src/app/inbox/email-resolver.service.ts
+++ src/app/inbox/email-resolver.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { EMPTY } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Email } from './email';
 import { EmailService } from './email.service';
@@ -15,7 +16,9 @@ export class EmailResolverService implements Resolve<Email>{
     const { id } = route.params;
 
     return this.emailService.getEmail(id).pipe(
+      catchError(() => {
 
+      })
     )
   }
 }
