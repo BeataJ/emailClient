dac559b33212490ed49d7d342e0ed8e085e3b22e
beataj <beata@emc2wave.com>
2021-04-19 18:29:43 -0700

    import EmailResolverService

--- src/app/inbox/inbox-routing.module.ts
+++ src/app/inbox/inbox-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmailShowComponent } from './email-show/email-show.component';
 import { HomeComponent } from './home/home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
+import { EmailResolverService } from './email-resolver.service';
 
 const routes: Routes = [
   {
