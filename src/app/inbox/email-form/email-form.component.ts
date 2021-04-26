03a2cdb560577ba6a311f459f5d559c18b5d1396
beataj <beata@emc2wave.com>
2021-04-25 17:05:26 -0700

    add Input email to email-form

--- src/app/inbox/email-form/email-form.component.ts
+++ src/app/inbox/email-form/email-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Email } from '../email';
 
 @Component({
   selector: 'app-email-form',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./email-form.component.css']
 })
 export class EmailFormComponent implements OnInit {
+  @Input() email: Email;
 
   constructor() { }
 
