52065473f85b9f715273a77379dbf57045db49b4
beataj <beata@emc2wave.com>
2021-04-27 16:09:33 -0700

    add email Input   email-show to email-replay

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Email } from '../email';
 
 
@@ -9,7 +9,7 @@ import { Email } from '../email';
 })
 export class EmailReplyComponent implements OnInit {
   showModal = false;
-  email: Email
+  @Input() email: Email
 
   constructor() { }
 
